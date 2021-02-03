3ee476ae9d4f55682539fafd36b3476b0203b5df
joon <jdj610@gmail.com>
2021-02-03 16:39:23 +0900

    update rollup setup

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from "rollup-plugin-terser";
+
 export default {
   input: './src/index.ts',
   output: {
     file: pkg.main,
     format: 'es'
   },
-  sourceMap: 'inline',
   plugins: [
     peerDepsExternal(),
     typescript(),
